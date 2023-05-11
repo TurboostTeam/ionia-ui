a3dfacb777992d6c9eec7a73fda89ee50c68899e
D4rkCr0w <me@d4rkcr0w.com>
2023-05-11 19:33:35 +0800

    feat: 使用 father 构建

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,12 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    {
-      name: "@storybook/addon-styling",
-      options: {
-        postCss: true,
-      },
-    },
+    "@storybook/addon-styling",
   ],
   framework: {
     name: "@storybook/react-vite",
