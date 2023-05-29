142f18580a45bcbfd56b17df5ab918d2978c2034
陈炫宗 <chenxuanzong@lf-network.com>
2023-05-29 10:48:14 +0800

    fix(table): 使用 react-table v8

--- src/graphql-table/GraphQLTable.stories.tsx
+++ src/graphql-table/GraphQLTable.stories.tsx
@@ -1,6 +1,7 @@
 import { type Meta } from "@storybook/react";
 import { type FC } from "react";
 
+import { type TableColumnProps } from "../table";
 import { GraphQLTable } from "./GraphQLTable";
 
 const meta = {
@@ -12,11 +13,22 @@ const meta = {
 export default meta;
 
 export const Controlled: FC = () => {
+  const columns: Array<TableColumnProps<any>> = [
+    { accessorKey: "name" },
+    { accessorKey: "age" },
+    { accessorKey: "year", footer: () => "123" },
+  ];
+
   return (
     <GraphQLTable
       loading
-      columns={[]}
-      edges={[]}
+      columns={columns}
+      edges={[
+        { node: { name: "1", age: 1, year: 2023 }, cursor: "1" },
+        { node: { name: "2", age: 2, year: 2023 }, cursor: "2" },
+        { node: { name: "3", age: 3, year: 2023 }, cursor: "3" },
+        { node: { name: "4", age: 4, year: 2023 }, cursor: "4" },
+      ]}
       emptyStateDescription="没有找到相关记录"
       emptyStateTitle="暂无数据"
     />
--- src/table/Table.stories.tsx
+++ src/table/Table.stories.tsx
@@ -11,36 +11,75 @@ const meta = {
 
 export default meta;
 
-interface Task {
+interface Person {
+  payload: {
+    a: number;
+    b: string[];
+  };
   name: string;
+  age: number;
+  visits: number;
   status: string;
+  progress: number;
 }
 
 export const Base: FC = () => {
   return (
-    <Table<Task>
+    <Table<Person>
       columns={[
         {
-          title: "Name",
-          field: "name",
+          header: "Name",
+          align: "right",
+          accessorKey: "name",
+          footer: () => "123",
         },
         {
-          title: "Status",
-          field: "status",
+          header: "Age",
+          accessorKey: "age",
+          footer: () => "123",
         },
+        {
+          header: "Visits",
+          accessorKey: "visits",
+          align: "center",
+          footer: () => "123",
+        },
+        {
+          header: "Status",
+          accessorKey: "status",
+          footer: () => "123",
+        },
+        {
+          header: "Progress",
+          accessorKey: "progress",
+          footer: () => "123",
+        },
+        { header: "Payload", accessorKey: "payload.a" },
       ]}
       data={[
         {
-          name: "Mehmet",
-          status: "started",
+          payload: { a: 1, b: ["1", "tanner"] },
+          name: "linsley",
+          age: 24,
+          visits: 100,
+          status: "In Relationship",
+          progress: 50,
         },
         {
-          name: "Mehmet",
-          status: "completed",
+          payload: { a: 2, b: ["2", "tandy"] },
+          name: "miller",
+          age: 40,
+          visits: 40,
+          status: "Single",
+          progress: 80,
         },
         {
-          name: "Mehmet",
-          status: "failed",
+          payload: { a: 3, b: ["3", "joe"] },
+          name: "dirte",
+          age: 45,
+          visits: 20,
+          status: "Complicated",
+          progress: 10,
         },
       ]}
     />
