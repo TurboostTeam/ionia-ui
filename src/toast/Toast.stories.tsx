f76e1a7f5a7650e318d6ff63d0848e8b6ffcd015
chenxuanzong <2622009279@qq.com>
2023-06-06 15:49:36 +0800

    fix(toast): 修改 toast

--- src/toast/Toast.stories.tsx
+++ src/toast/Toast.stories.tsx
@@ -1,32 +1,23 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta } from "@storybook/react";
 import { type FC } from "react";
 
 import { Button } from "../button";
 import { Provider } from "../provider";
-import { Toast } from "./Toast";
-import { toast } from "./toast.util";
+import { toast } from "./Toast";
 
 const meta = {
   title: "Feedback/Toast",
-  component: Toast,
   tags: ["autodocs"],
-} satisfies Meta<typeof Toast>;
+} satisfies Meta<typeof toast>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
-
-export const Default: Story = {
-  args: {
-    children: "message",
-  },
-};
 
 export const Dynamic: FC = () => {
   return (
     <Provider>
       <Button
         onClick={() => {
-          toast("message");
+          toast.success("message");
         }}
       >
         Toast
--- src/toast/Toast.tsx
+++ src/toast/Toast.tsx
@@ -1,24 +1,74 @@
-import { XMarkIcon } from "@heroicons/react/24/outline";
-import { type FC, type ReactNode } from "react";
+import {
+  type DefaultToastOptions,
+  type Renderable,
+  type Toast,
+  toast as baseToast,
+  type ToastOptions,
+  type ValueOrFunction,
+} from "react-hot-toast";
 
-export interface ToastProps {
-  children?: ReactNode;
-  onDismiss?: () => void;
+enum IconType {
+  INFO,
+  WARN,
 }
 
-export const Toast: FC<ToastProps> = ({ children, onDismiss }) => {
-  return (
-    <div className="inline-block">
-      <div className="flex w-auto items-center justify-between gap-1 rounded-lg bg-gray-800 p-3 text-sm text-gray-50 shadow-lg">
-        <div>{children}</div>
+type Message = ValueOrFunction<Renderable, Toast>;
+type ToastHandler = (message: Message, options?: ToastOptions) => string;
 
-        <XMarkIcon
-          className="-mr-1 h-4 w-4 cursor-pointer"
-          onClick={() => {
-            onDismiss?.();
-          }}
-        />
-      </div>
-    </div>
-  );
-};
+interface baseToastProps {
+  (message: Message, opts?: ToastOptions): string;
+  error: ToastHandler;
+  success: ToastHandler;
+  loading: ToastHandler;
+  custom: ToastHandler;
+  dismiss: (toastId?: string) => void;
+  remove: (toastId?: string) => void;
+  promise: <T>(
+    promise: Promise<T>,
+    msgs: {
+      loading: Renderable;
+      success: ValueOrFunction<Renderable, T>;
+      error: ValueOrFunction<Renderable, any>;
+    },
+    opts?: DefaultToastOptions
+  ) => Promise<T>;
+}
+
+interface toastProps extends baseToastProps {
+  info: ToastHandler;
+  warn: ToastHandler;
+}
+
+const infoIcon = (type: IconType): Renderable => (
+  <svg
+    aria-hidden="true"
+    color={type === IconType.INFO ? "#1677ff" : "#faac14"}
+    data-icon="exclamation-circle"
+    fill="currentColor"
+    focusable="false"
+    height="1em"
+    viewBox="64 64 896 896"
+    width="1em"
+  >
+    <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm-32 232c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V296zm32 440a48.01 48.01 0 010-96 48.01 48.01 0 010 96z" />
+  </svg>
+);
+
+const infoToast = (message: Message, options?: ToastOptions): string =>
+  baseToast.error(message, {
+    icon: infoIcon(IconType.INFO),
+    ...options,
+  });
+
+const warnToast = (message: Message, options?: ToastOptions): string =>
+  baseToast.error(message, {
+    icon: infoIcon(IconType.WARN),
+    ...options,
+  });
+
+const toast = baseToast as toastProps;
+
+toast.info = infoToast;
+toast.warn = warnToast;
+
+export { toast };
