7ccf709b5ad22c19369a3ddf3ed1f45f01f93a48
D4rkCr0w <me@d4rkcr0w.com>
2023-06-08 23:56:07 +0800

    fix: 修复 RadioGroup 和 CheckboxGroup 的 ref

--- src/checkbox-group/CheckboxGroup.tsx
+++ src/checkbox-group/CheckboxGroup.tsx
@@ -1,61 +1,53 @@
-import { type ReactElement } from "react";
-
 import { Checkbox } from "../checkbox";
-import { FormItem, type FormItemProps } from "../form-item/FormItem";
+import { forwardRef } from "../common";
+import { FormItem, type FormItemProps } from "../form-item";
 
-export interface CheckboxGroupOption<T> {
+export interface CheckboxGroupOption {
   label: string;
-  value: T;
+  value: string;
 }
 
-export interface CheckboxGroupProps<T> extends FormItemProps {
-  options: Array<CheckboxGroupOption<T>>;
-  value?: T[];
-  onChange?: (value: T[]) => void;
+export interface CheckboxGroupProps extends FormItemProps {
+  options: CheckboxGroupOption[];
+  value?: string[];
+  onChange?: (value: string[]) => void;
 }
 
-export function CheckboxGroup<T extends string>({
-  className,
-  label,
-  helpText,
-  error,
-  options,
-  value,
-  onChange,
-  ...props
-}: CheckboxGroupProps<T>): ReactElement {
-  return (
-    <FormItem
-      className={className}
-      error={error}
-      helpText={helpText}
-      label={label}
-      {...props}
-    >
-      {options.map((option) => (
-        <Checkbox
-          checked={
-            typeof onChange !== "undefined"
-              ? value?.includes(option.value) ?? false
-              : undefined
-          }
-          key={option.label}
-          label={option.label}
-          onChange={
-            typeof onChange !== "undefined"
-              ? (event) => {
-                  if (event.target.checked) {
-                    onChange([...(value ?? []), option.value]);
-                  } else {
-                    if (typeof value !== "undefined") {
-                      onChange(value.filter((item) => item !== option.value));
+export const CheckboxGroup = forwardRef<CheckboxGroupProps, "div">(
+  ({ className, label, helpText, error, options, value, onChange }, ref) => {
+    return (
+      <FormItem
+        className={className}
+        error={error}
+        helpText={helpText}
+        label={label}
+        ref={ref}
+      >
+        {options.map((option) => (
+          <Checkbox
+            checked={
+              typeof onChange !== "undefined"
+                ? value?.includes(option.value) ?? false
+                : undefined
+            }
+            key={option.label}
+            label={option.label}
+            onChange={
+              typeof onChange !== "undefined"
+                ? (event) => {
+                    if (event.target.checked) {
+                      onChange([...(value ?? []), option.value]);
+                    } else {
+                      if (typeof value !== "undefined") {
+                        onChange(value.filter((item) => item !== option.value));
+                      }
                     }
                   }
-                }
-              : undefined
-          }
-        />
-      ))}
-    </FormItem>
-  );
-}
+                : undefined
+            }
+          />
+        ))}
+      </FormItem>
+    );
+  }
+);
--- src/form-item/FormItem.tsx
+++ src/form-item/FormItem.tsx
@@ -1,16 +1,9 @@
-import {
-  type DetailedHTMLProps,
-  type FC,
-  type HTMLAttributes,
-  type ReactNode,
-} from "react";
+import { type ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
 
-export interface FormItemProps
-  extends Omit<
-    DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>,
-    "value" | "onChange"
-  > {
+import { forwardRef } from "../common";
+
+export interface FormItemProps {
   children?: ReactNode;
   className?: string;
   label?: string;
@@ -18,45 +11,40 @@ export interface FormItemProps
   error?: string;
 }
 
-export const FormItem: FC<FormItemProps> = ({
-  children,
-  className,
-  label,
-  helpText,
-  error,
-  ...props
-}) => {
-  return (
-    <div className="w-full" {...props}>
-      {typeof label !== "undefined" && (
-        <label
-          className={twMerge(
-            "block font-medium leading-6 text-gray-900 text-sm",
-            className
-          )}
-          htmlFor={label}
-        >
-          {label}
-        </label>
-      )}
+export const FormItem = forwardRef<FormItemProps, "div">(
+  ({ children, className, label, helpText, error }, ref) => {
+    return (
+      <div className="w-full" ref={ref}>
+        {typeof label !== "undefined" && (
+          <label
+            className={twMerge(
+              "block font-medium leading-6 text-gray-900 text-sm",
+              className
+            )}
+            htmlFor={label}
+          >
+            {label}
+          </label>
+        )}
 
-      <div className={twMerge(typeof label !== "undefined" && "mt-2")}>
-        {children}
+        <div className={twMerge(typeof label !== "undefined" && "mt-2")}>
+          {children}
+        </div>
+
+        {(typeof error !== "undefined" || typeof helpText !== "undefined") && (
+          <p
+            className={twMerge(
+              `mt-2 text-sm`,
+              typeof error !== "undefined" && `text-red-600`,
+              typeof error === "undefined" &&
+                typeof helpText !== "undefined" &&
+                `text-gray-500`
+            )}
+          >
+            {error ?? helpText}
+          </p>
+        )}
       </div>
-
-      {(typeof error !== "undefined" || typeof helpText !== "undefined") && (
-        <p
-          className={twMerge(
-            `mt-2 text-sm`,
-            typeof error !== "undefined" && `text-red-600`,
-            typeof error === "undefined" &&
-              typeof helpText !== "undefined" &&
-              `text-gray-500`
-          )}
-        >
-          {error ?? helpText}
-        </p>
-      )}
-    </div>
-  );
-};
+    );
+  }
+);
