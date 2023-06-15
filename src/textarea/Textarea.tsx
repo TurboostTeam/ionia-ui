8148f5a4a57b29650167c67c11414c323ce1798e
D4rkCr0w <me@d4rkcr0w.com>
2023-06-15 18:10:30 +0800

    fix(Textarea): className 传递到 textarea

--- src/textarea/Textarea.tsx
+++ src/textarea/Textarea.tsx
@@ -26,18 +26,14 @@ export const Textarea = forwardRef<TextareaProps, "textarea">(
     ref
   ) => {
     return (
-      <FormItem
-        className={className}
-        error={error}
-        helpText={helpText}
-        label={label}
-      >
+      <FormItem error={error} helpText={helpText} label={label}>
         <textarea
           className={twMerge(
             "rounded-md px-3 py-2 shadow-sm min-w-0 border-0 bg-inherit text-sm disabled:cursor-not-allowed w-full ring-1 ring-inset focus-within:bg-gray-50 focus:text-gray-500 focus:ring-2 focus:ring-inset ring-gray-300 focus:ring-indigo-600 placeholder:text-gray-400 text-gray-900",
             typeof label !== "undefined" && "mt-2",
             typeof error !== "undefined" &&
-              `ring-red-300 focus:ring-red-500 placeholder:text-red-300 text-red-900`
+              `ring-red-300 focus:ring-red-500 placeholder:text-red-300 text-red-900`,
+            className
           )}
           ref={ref}
           rows={rows}
