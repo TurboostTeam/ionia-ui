59645a649f6134d0889fcce682a0d9a088d88514
D4rkCr0w <me@d4rkcr0w.com>
2023-08-04 14:12:08 +0800

    fix(Switch): 细节调整

--- src/Switch/Switch.tsx
+++ src/Switch/Switch.tsx
@@ -23,7 +23,7 @@ export const Switch = forwardRef<SwitchProps, "input">(
           checked={checked}
           className={twMerge(
             checked === true ? "bg-indigo-600" : "bg-gray-200",
-            "relative inline-flex h-8 w-14 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out"
+            "relative inline-flex h-8 w-14 flex-shrink-0 cursor-pointer rounded-full transition-colors duration-200 ease-in-out p-0.5",
           )}
           ref={ref}
           {...props}
@@ -32,11 +32,11 @@ export const Switch = forwardRef<SwitchProps, "input">(
             aria-hidden="true"
             className={twMerge(
               checked === true ? "translate-x-6" : "translate-x-0",
-              "pointer-events-none inline-block h-7 w-7 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
+              "pointer-events-none inline-block h-7 w-7 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out",
             )}
           />
         </BaseSwitch>
       </FormItem>
     );
-  }
+  },
 );
