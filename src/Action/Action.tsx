8bb9f7b2367a27aac7d3da98ad8904122ad2f7a4
D4rkCr0w <me@d4rkcr0w.com>
2023-08-07 15:14:24 +0800

    fix(Action): 调整类型

--- src/Action/Action.tsx
+++ src/Action/Action.tsx
@@ -3,13 +3,14 @@ import { type FC, type ReactNode } from "react";
 import { Button, type ButtonProps } from "../Button";
 import { forwardRef } from "../utils";
 
-export interface ActionProps extends Omit<ButtonProps, "children"> {
+export interface ActionProps extends Omit<ButtonProps, "children" | "onClick"> {
   content: ReactNode;
+  onAction?: () => Promise<void> | void;
 }
 
-export const Action: FC<ActionProps> = forwardRef<ActionProps, "button">(
-  ({ content, ...props }, ref) => (
-    <Button ref={ref} {...props}>
+export const Action: FC<ActionProps> = forwardRef<ActionProps, "div">(
+  ({ content, onAction, ...props }, ref) => (
+    <Button ref={ref} onClick={onAction} {...props}>
       {content}
     </Button>
   ),
