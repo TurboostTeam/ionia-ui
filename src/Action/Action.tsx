2807da8fd7f2f8c8608fb662f58fbd0ad7b45f1f
D4rkCr0w <me@d4rkcr0w.com>
2023-08-10 01:12:16 +0800

    fix(Action): Action 支持 as 参数

--- src/Action/Action.tsx
+++ src/Action/Action.tsx
@@ -1,16 +1,14 @@
-import { type FC, type ReactNode } from "react";
+import { type ReactNode } from "react";
 
 import { Button, type ButtonProps } from "../Button";
-import { type As } from "../types";
 import { forwardRef } from "../utils";
 
-export interface ActionProps extends Omit<ButtonProps, "children" | "onClick"> {
-  as?: As;
-  content: ReactNode;
+export interface ActionProps extends ButtonProps {
+  content?: ReactNode;
   onAction?: () => Promise<void> | void;
 }
 
-export const Action: FC<ActionProps> = forwardRef<ActionProps, "div">(
+export const Action = forwardRef<ActionProps, "button">(
   ({ content, onAction, ...props }, ref) => (
     <Button ref={ref} onClick={onAction} {...props}>
       {content}
