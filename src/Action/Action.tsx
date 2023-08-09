865fb5648e20c08de6a2d08533fd35f403d5f915
D4rkCr0w <me@d4rkcr0w.com>
2023-08-10 00:01:02 +0800

    fix(Action): Action 支持 as 参数

--- src/Action/Action.tsx
+++ src/Action/Action.tsx
@@ -1,9 +1,11 @@
 import { type FC, type ReactNode } from "react";
 
 import { Button, type ButtonProps } from "../Button";
+import { type As } from "../types";
 import { forwardRef } from "../utils";
 
 export interface ActionProps extends Omit<ButtonProps, "children" | "onClick"> {
+  as?: As;
   content: ReactNode;
   onAction?: () => Promise<void> | void;
 }
