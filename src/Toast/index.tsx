3398d00914fb9b2bc655137455e15c4bdbfb6852
D4rkCr0w <me@d4rkcr0w.com>
2023-08-11 02:07:19 +0800

    fix: 删除 toast

--- src/Toast/index.tsx
+++
@@ -1,5 +0,0 @@
-export { Toast } from "./Toast";
-export type { ToastProps } from "./ToastProps";
-export { ToastProvider } from "./ToastProvider";
-// eslint-disable-next-line react-refresh/only-export-components
-export { useToast } from "./useToast";
