3376d544c34d04da4472324cc72322245294de96
linqiuyue <38214938+linqiuyue@users.noreply.github.com>
2023-08-18 15:20:07 +0800

    feat: 新增上下文保存条组件 (#2)
    * feat: 新增上下文保存条组件
    
    * fix: 修改
    
    * fix: 修改
    
    * fix: 修改
    
    ---------
    
    Co-authored-by: linqiuyue <me@linqiuyue.com>

---
+++ src/ContextualSaveBar/ContextualSaveBar.stories.tsx
@@ -0,0 +1,120 @@
+import type { Meta } from "@storybook/react";
+import { type FC } from "react";
+
+import { Card } from "../Card";
+import { Page } from "../Page";
+import { ContextualSaveBar } from "./ContextualSaveBar";
+import page from "./ContextualSaveBar.mdx";
+
+const meta = {
+  title: "Layout 布局/ContextualSaveBar 上下文保存条",
+  component: ContextualSaveBar,
+  parameters: {
+    docs: {
+      page,
+    },
+  },
+} satisfies Meta<typeof ContextualSaveBar>;
+
+export default meta;
+export const Default: FC = (args) => {
+  return (
+    <div className="bg-gray-100">
+      <ContextualSaveBar
+        discardAction={{
+          content: "放弃",
+          onAction: () => {
+            console.log("放弃");
+          },
+        }}
+        message="保存条"
+        saveAction={{
+          content: "保存",
+          onAction: () => {
+            console.log("保存");
+          },
+        }}
+        {...args}
+      />
+      <div className="mt-14">
+        <Page
+          backAction={{}}
+          primaryAction={{ content: "创建" }}
+          secondaryActions={[{ content: "设置" }]}
+          title="标题"
+        >
+          <Card className="h-72">页面内容</Card>
+        </Page>
+      </div>
+    </div>
+  );
+};
+
+export const FullWidth: FC = (args) => {
+  return (
+    <div className="bg-gray-100">
+      <ContextualSaveBar
+        fullWidth
+        discardAction={{
+          content: "放弃",
+          onAction: () => {
+            console.log("放弃");
+          },
+        }}
+        message="保存条"
+        saveAction={{
+          content: "保存",
+          onAction: () => {
+            console.log("保存");
+          },
+        }}
+        {...args}
+      />
+      <div className="mt-14">
+        <Page
+          fullWidth
+          backAction={{}}
+          primaryAction={{ content: "创建" }}
+          secondaryActions={[{ content: "设置" }]}
+          title="标题"
+        >
+          <Card className="h-72">页面内容</Card>
+        </Page>
+      </div>
+    </div>
+  );
+};
+
+export const NoMessage: FC = (args) => {
+  return (
+    <div className="bg-gray-100">
+      <ContextualSaveBar
+        fullWidth
+        discardAction={{
+          content: "放弃",
+          onAction: () => {
+            console.log("放弃");
+          },
+        }}
+        saveAction={{
+          content: "保存",
+          onAction: () => {
+            console.log("保存");
+          },
+        }}
+        {...args}
+      />
+      <div className="mt-14">
+        <Page
+          fullWidth
+          backAction={{}}
+          primaryAction={{ content: "创建" }}
+          secondaryActions={[{ content: "设置" }]}
+          title="标题"
+        >
+          <Card className="h-72">页面内容</Card>
+        </Page>
+      </div>
+    </div>
+  );
+};
---
+++ src/ContextualSaveBar/ContextualSaveBar.tsx
@@ -0,0 +1,40 @@
+import { type FC } from "react";
+import { twMerge } from "tailwind-merge";
+
+import { Action, type ActionProps } from "../Action";
+import { ButtonGroup } from "../ButtonGroup";
+
+export interface ContextualSaveBarProps {
+  message?: string;
+  saveAction: ActionProps;
+  discardAction?: ActionProps;
+  fullWidth?: boolean;
+}
+
+export const ContextualSaveBar: FC<ContextualSaveBarProps> = ({
+  message,
+  saveAction,
+  discardAction,
+  fullWidth = false,
+}) => {
+  return (
+    <div className="fixed left-0 top-0 z-50 w-full bg-black">
+      <div
+        className={twMerge(
+          `flex h-14 items-center justify-end  px-4 mx-auto`,
+          !fullWidth && `max-w-5xl`,
+          typeof message !== "undefined" && `justify-between`,
+        )}
+      >
+        {typeof message !== "undefined" && (
+          <h2 className="text-gray-50">{message}</h2>
+        )}
+
+        <ButtonGroup>
+          {discardAction != null && <Action {...discardAction} />}
+          <Action primary {...saveAction} />
+        </ButtonGroup>
+      </div>
+    </div>
+  );
+};
