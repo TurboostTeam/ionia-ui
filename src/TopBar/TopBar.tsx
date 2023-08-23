30210ac57b4693fa22cdb9817dbb5f74582e88e3
D4rkCr0w <me@d4rkcr0w.com>
2023-08-23 18:24:50 +0800

    feat: TopBar 添加 UserMenu

--- src/TopBar/TopBar.tsx
+++ src/TopBar/TopBar.tsx
@@ -1,14 +1,24 @@
 import { Bars3Icon } from "@heroicons/react/24/outline";
 import { type FC } from "react";
 
+import { type ActionProps } from "../Action";
 import { Button } from "../Button";
+import { Popover } from "../Popover";
+
+export interface TopBarUserMenuProps {
+  name: string;
+  avatar?: string;
+  actions?: ActionProps[];
+}
 
 export interface TopBarProps {
+  userMenu?: TopBarUserMenuProps;
   showNavigationToggle?: boolean;
   onNavigationToggle?: () => void;
 }
 
 export const TopBar: FC<TopBarProps> = ({
+  userMenu,
   showNavigationToggle = false,
   onNavigationToggle,
 }) => {
@@ -27,7 +37,31 @@ export const TopBar: FC<TopBarProps> = ({
         )}
       </div>
       <div className="flex justify-center"> </div>
-      <div className="flex justify-end"> </div>
+      <div className="flex justify-end">
+        {userMenu != null && (
+          <Popover
+            activator={
+              <button className="flex cursor-pointer items-center justify-center gap-1 rounded p-0.5 text-sm hover:bg-gray-100">
+                <div className="max-w-[theme(spacing.40)] truncate px-2">
+                  {userMenu.name}
+                </div>
+
+                <div className="h-7 w-7 overflow-hidden rounded bg-gray-300">
+                  {typeof userMenu.avatar !== "undefined" ? (
+                    <img alt={userMenu.name} src={userMenu.avatar} />
+                  ) : (
+                    <div className="flex h-full w-full items-center justify-center">
+                      {userMenu.name.slice(0, 1)}
+                    </div>
+                  )}
+                </div>
+              </button>
+            }
+          >
+            {/* {userMenu.actions.map((action) => ()} */}
+          </Popover>
+        )}
+      </div>
     </div>
   );
 };
