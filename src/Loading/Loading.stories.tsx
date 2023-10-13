bdf7da3b4ab0ddf5463e27fca0b8585ff0750355
linqiuyue <me@linqiuyue.com>
2023-10-13 17:12:39 +0800

    fix: 修改

--- src/Loading/Loading.stories.tsx
+++ src/Loading/Loading.stories.tsx
@@ -2,11 +2,19 @@ import type { Meta } from "@storybook/react";
 import React, { type FC } from "react";
 
 import { Loading } from "./Loading";
+import page from "./Loading.mdx";
 
 export default {
   title: "Layout 布局/Loading 加载",
+  component: Loading,
   parameters: {
-    layout: "fullscreen",
+    backgrounds: {
+      default: "gray",
+      values: [{ name: "gray", value: "rgb(249, 250, 251)" }],
+    },
+    docs: {
+      page,
+    },
   },
 } satisfies Meta<typeof Loading>;
 
