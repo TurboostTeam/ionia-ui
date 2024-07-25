0ca5052d7cb02a2b239497b96307be54de190a5b
linqiuyue <me@linqiuyue.com>
2024-07-25 12:28:49 +0800

    fix: 修改

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,6 @@
 import { type Config } from "tailwindcss";
 
 export default {
-  // theme: {
-  //   extend: {
-  //     colors: {
-  //       primary: "var(--primary-color)",
-  //       secondary: "var(--secondary-color)",
-  //       success: "var(--success-color)",
-  //       warning: "var(--warning-color)",
-  //       danger: "var(--danger-color)",
-  //       info: "var(--info-color)",
-  //       light: "var(--light-color)",
-  //     },
-  //   },
-  // },
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   presets: [require("./preset")],
 } satisfies Config;
