64e5975cd5bf93568504af7ed85cd0eb16c07092
chenxuanzong <2622009279@qq.com>
2024-08-13 15:00:37 +0800

    feat(topbar): 新增 logo 接口

--- src/TopBar/TopBar.tsx
+++ src/TopBar/TopBar.tsx
@@ -5,29 +5,30 @@ import { Button } from "../Button";
 
 export interface TopBarProps {
   userMenu?: ReactElement;
+  logo?: ReactElement;
   showNavigationToggle?: boolean;
   onNavigationToggle?: () => void;
 }
 
 export const TopBar: FC<TopBarProps> = ({
   userMenu,
+  logo,
   showNavigationToggle = false,
   onNavigationToggle,
 }) => {
   return (
-    <div className="grid h-14 grid-cols-[1fr_minmax(auto,30rem)_1fr] items-center gap-1 border-b bg-white px-4">
-      <div className="flex justify-start">
-        {showNavigationToggle && (
-          <Button
-            className="block md:hidden"
-            icon={Bars3Icon}
-            variant="ghost"
-            onClick={onNavigationToggle}
-          />
-        )}
-      </div>
-      <div className="flex justify-center"> </div>
-      <div className="flex justify-end">{userMenu}</div>
+    <div className="flex h-14 items-center gap-4 border-b bg-white px-4">
+      {showNavigationToggle && (
+        <Button
+          className="block md:hidden"
+          icon={Bars3Icon}
+          variant="ghost"
+          onClick={onNavigationToggle}
+        />
+      )}
+
+      {logo}
+      <div className="ml-auto">{userMenu}</div>
     </div>
   );
 };
