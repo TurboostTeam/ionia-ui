3d0e0a306567df33577283f39c350c9faf12b421
Xudong Huang <me@xudong.cc>
2024-08-27 21:16:16 +0800

    fix: 修复可交互组件聚焦光环

--- src/atoms/input/input.tsx
+++ src/atoms/input/input.tsx
@@ -2,7 +2,7 @@ import { forwardRef, type InputHTMLAttributes } from "react";
 import { tv, type VariantProps } from "tailwind-variants";
 
 export const input = tv({
-  base: "border-input placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
+  base: "border-input placeholder:text-muted-foreground flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-focus disabled:cursor-not-allowed disabled:opacity-50",
 });
 
 export interface InputProps
