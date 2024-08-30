6b5d3f1adfe11b3fdcfff74507bb89ae9ffd9ea8
Xudong Huang <me@xudong.cc>
2024-08-30 11:23:02 +0800

    fix: 优化 PageLayout 组件实现，调整 Card 组件在 md 下就取消圆角

--- src/atoms/card/card.tsx
+++ src/atoms/card/card.tsx
@@ -2,7 +2,7 @@ import { forwardRef, type HTMLAttributes } from "react";
 import { tv } from "tailwind-variants";
 
 export const card = tv({
-  base: "flex flex-col gap-2 bg-surface p-4 shadow sm:rounded-lg",
+  base: "flex flex-col gap-2 bg-surface p-4 shadow md:rounded-lg",
 });
 
 export interface CardProps extends HTMLAttributes<HTMLDivElement> {}
--- src/page-layout/page-layout.tsx
+++ src/page-layout/page-layout.tsx
@@ -1,5 +1,14 @@
-import { type FC, type PropsWithChildren } from "react";
+import { forwardRef, type HTMLAttributes } from "react";
+import { tv, type VariantProps } from "tailwind-variants";
 
-export const PageLayout: FC<PropsWithChildren> = ({ children }) => {
-  return <div className="flex flex-wrap gap-4">{children}</div>;
-};
+export const pageLayout = tv({
+  base: "grid grid-cols-3 gap-4",
+});
+
+export interface PageLayoutProps
+  extends HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof pageLayout> {}
+
+export const PageLayout = forwardRef<HTMLDivElement, PageLayoutProps>(
+  (props, ref) => <div ref={ref} {...props} className={pageLayout(props)} />,
+);
