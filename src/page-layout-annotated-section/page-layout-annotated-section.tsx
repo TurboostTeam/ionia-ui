a902dec1e4f44a0ca449db588a2e042a8bc447b6
Xudong Huang <me@xudong.cc>
2024-08-30 11:23:02 +0800

    fix: 优化 PageLayout 组件实现，调整 Card 组件在 md 下就取消圆角

--- src/page-layout-annotated-section/page-layout-annotated-section.tsx
+++ src/page-layout-annotated-section/page-layout-annotated-section.tsx
@@ -1,5 +1,6 @@
 import { type FC, type PropsWithChildren } from "react";
-import { twMerge } from "tailwind-merge";
+
+import { PageLayoutSection } from "../page-layout-section";
 
 export interface PageLayoutAnnotatedSectionProps {
   title?: string;
@@ -8,19 +9,12 @@ export interface PageLayoutAnnotatedSectionProps {
 
 export const PageLayoutAnnotatedSection: FC<
   PropsWithChildren<PageLayoutAnnotatedSectionProps>
-> = ({ title, description, children }) => {
-  return (
-    <div className={twMerge(`flex flex-1 basis-full min-w-0 gap-4 flex-wrap`)}>
-      <div className="min-w-0 flex-[1] basis-full py-5 md:basis-0">
-        <h2 className="text-base font-semibold">{title}</h2>
-        <div className="mt-4 text-sm text-description">
-          <p>{description}</p>
-        </div>
-      </div>
-
-      <div className="min-w-[51%] flex-[2] basis-full md:basis-0">
-        {children}
-      </div>
-    </div>
-  );
-};
+> = ({ title, description, children }, ref) => (
+  <>
+    <PageLayoutSection variant="secondary">
+      <h2 className="text-base font-semibold">{title}</h2>
+      <p className="mt-4 text-sm text-description">{description}</p>
+    </PageLayoutSection>
+    <PageLayoutSection variant="primary">{children}</PageLayoutSection>
+  </>
+);
