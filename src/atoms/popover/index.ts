a592c4b611adab788d05d4f21cf97e4b6f9324a5
linqiuyue <me@linqiuyue.com>
2024-09-02 15:08:45 +0800

    fix: 移除关闭按钮和箭头

--- src/atoms/popover/index.ts
+++ src/atoms/popover/index.ts
@@ -1,7 +1,3 @@
 export * from "./popover";
 export * from "./popover-content";
 export * from "./popover-trigger";
-export {
-  Arrow as PopoverArrow,
-  Close as PopoverClose,
-} from "@radix-ui/react-popover";
--- src/atoms/popover/popover.stories.tsx
+++ src/atoms/popover/popover.stories.tsx
@@ -2,13 +2,7 @@ import type { Meta } from "@storybook/react";
 import type { FC } from "react";
 
 import { Button } from "../button";
-import {
-  Popover,
-  PopoverArrow,
-  PopoverClose,
-  PopoverContent,
-  PopoverTrigger,
-} from ".";
+import { Popover, PopoverContent, PopoverTrigger } from ".";
 
 const meta: Meta<typeof Popover> = {
   title: "Atoms 原子组件/Overlay 叠层/Popover 弹出窗口",
@@ -23,25 +17,13 @@ export const Base: FC = () => {
       <PopoverTrigger>
         <Button>Open</Button>
       </PopoverTrigger>
-      <PopoverContent
-        className="border-none"
-        side="bottom"
-        style={{
-          boxShadow:
-            "hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px",
-        }}
-      >
+      <PopoverContent side="bottom">
         <div className="space-y-2">
           <h4 className="font-medium leading-none">Dimensions</h4>
           <p className="text-muted-foreground text-sm">
             Set the dimensions for the layer.
           </p>
         </div>
-
-        <PopoverClose>
-          <Button>Close</Button>
-        </PopoverClose>
-        <PopoverArrow className="fill-white" />
       </PopoverContent>
     </Popover>
   );
