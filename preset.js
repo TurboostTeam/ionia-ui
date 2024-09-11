77be3a860d8f1423aaf16d0fcfaa9ac153b18e1b
Xudong Huang <me@xudong.cc>
2024-09-11 16:36:20 +0800

    fix: 修复不支持 tw 颜色透明度语法

--- preset.js
+++ preset.js
@@ -23,140 +23,136 @@ module.exports = {
 
       backgroundColor: {
         // 默认背景颜色，运动于大面积元素（如卡片）。
-        default: "var(--bg-default)",
+        default: "rgb(var(--bg-default))",
         // 用于突出度最高的元素的背景颜色，如卡片。
         surface: {
-          DEFAULT: "var(--bg-surface-default)",
-          hover: "var(--bg-surface-hover)",
-          active: "var(--bg-surface-active)",
-          info: "var(--bg-surface-info)",
-          transparent: "var(--bg-surface-transparent)",
+          DEFAULT: "rgb(var(--bg-surface-default))",
+          hover: "rgb(var(--bg-surface-hover))",
+          active: "rgb(var(--bg-surface-active))",
+          info: "rgb(var(--bg-surface-info))",
+          transparent: "rgb(var(--bg-surface-transparent))",
           // 主要用于显示小部分内容的背景颜色，如聊天气泡。
-          secondary: "var(--bg-surface-secondary)",
+          secondary: "rgb(var(--bg-surface-secondary))",
           // 用于背景，指示编辑器的重点领域，例如主题编辑器，input。
           emphasis: {
-            DEFAULT: "var(--bg-emphasis-default)",
-            hover: "var(--bg-emphasis-hover)",
-            active: "var(--bg-emphasis-active)",
+            DEFAULT: "rgb(var(--bg-emphasis-default))",
+            hover: "rgb(var(--bg-emphasis-hover))",
+            active: "rgb(var(--bg-emphasis-active))",
           },
         },
 
-        facebook: "var(--bg-facebook)",
-
         // 用于导航栏的背景颜色。
         nav: {
-          DEFAULT: "var(--bg-nav-default)",
+          DEFAULT: "rgb(var(--bg-nav-default))",
           // 用于导航栏浅表色
           surface: {
-            DEFAULT: "var(--bg-nav-surface-default)",
-            hover: "var(--bg-nav-surface-hover)",
-            active: "var(--bg-nav-surface-active)",
+            DEFAULT: "rgb(var(--bg-nav-surface-default))",
+            hover: "rgb(var(--bg-nav-surface-hover))",
+            active: "rgb(var(--bg-nav-surface-active))",
           },
         },
         // 所包含表面积较小的元素（如按钮）的背景颜色。
         fill: {
           // 主要操作（如主要按钮）的背景颜色。
           primary: {
-            DEFAULT: "var(--bg-fill-primary-default)",
-            secondary: "var(--bg-fill-primary-secondary)",
-            hover: "var(--bg-fill-primary-hover)",
-            active: "var(--bg-fill-primary-active)",
+            DEFAULT: "rgb(var(--bg-fill-primary-default))",
+            secondary: "rgb(var(--bg-fill-primary-secondary))",
+            hover: "rgb(var(--bg-fill-primary-hover))",
+            active: "rgb(var(--bg-fill-primary-active))",
             // selected: "",
           },
           // 次要操作（如次要按钮）的背景颜色。
           secondary: {
-            DEFAULT: "var(--bg-fill-secondary-default)",
-            hover: "var(--bg-fill-secondary-hover)",
-            active: "var(--bg-fill-secondary-active)",
+            DEFAULT: "rgb(var(--bg-fill-secondary-default))",
+            hover: "rgb(var(--bg-fill-secondary-hover))",
+            active: "rgb(var(--bg-fill-secondary-active))",
             // active: "",
             // selected: "",
           },
           // 警告操作（如警告按钮）的背景颜色。
           destructive: {
-            DEFAULT: "var(--bg-fill-destructive-default)",
-            hover: "var(--bg-fill-destructive-hover)",
-            active: "var(--bg-fill-destructive-active)",
+            DEFAULT: "rgb(var(--bg-fill-destructive-default))",
+            hover: "rgb(var(--bg-fill-destructive-hover))",
+            active: "rgb(var(--bg-fill-destructive-active))",
             // selected: "",
           },
           // 禁用状态的背景颜色。
-          disabled: "var(--bg-fill-disabled)",
+          disabled: "rgb(var(--bg-fill-disabled))",
           // 用于表面的背景颜色。
           transparent: {
-            DEFAULT: "var(--bg-fill-transparent-default)",
-            hover: "var(--bg-fill-transparent-hover)",
+            DEFAULT: "rgb(var(--bg-fill-transparent-default))",
+            hover: "rgb(var(--bg-fill-transparent-hover))",
           },
         },
       },
       textColor: {
         // 用于填充背景颜色下的文本颜色。
         fill: {
-          primary: "var(--text-fill-primary)",
-          secondary: "var(--text-fill-secondary)",
-          destructive: "var(--text-fill-destructive)",
+          primary: "rgb(var(--text-fill-primary))",
+          secondary: "rgb(var(--text-fill-secondary))",
+          destructive: "rgb(var(--text-fill-destructive))",
         },
         // 用于链接文本的颜色。
         link: {
-          DEFAULT: "var(--text-link-default)",
-          hover: "var(--text-link-hover)",
-          active: "var(--text-link-active)",
+          DEFAULT: "rgb(var(--text-link-default))",
+          hover: "rgb(var(--text-link-hover))",
+          active: "rgb(var(--text-link-active))",
         },
         // 用于默认文本的颜色。
-        default: "var(--text-default)",
+        default: "rgb(var(--text-default))",
 
         // 用于占位符的文本颜色。
-        placeholder: "var(--text-placeholder)",
-
-        facebook: "var(--text-facebook)",
+        placeholder: "rgb(var(--text-placeholder))",
 
         // 用于主要比如 radio ,checkbox 的 input选中状态下的文本颜色。
-        primary: "var(--text-primary)",
+        primary: "rgb(var(--text-primary))",
 
         // 用于次要文本的颜色。
-        secondary: "var(--text-secondary)",
+        secondary: "rgb(var(--text-secondary))",
 
         // 用于提示的文本颜色。
         destructive: {
-          DEFAULT: "var(--text-destructive-default)",
-          placeholder: "var(--text-destructive-placeholder)",
+          DEFAULT: "rgb(var(--text-destructive-default))",
+          placeholder: "rgb(var(--text-destructive-placeholder))",
         },
 
         // 用于禁用状态的文本颜色。
-        disabled: "var(--text-disabled)",
+        disabled: "rgb(var(--text-disabled))",
 
         // 用于描述性文本的颜色。
-        description: "var(--text-description)",
+        description: "rgb(var(--text-description))",
 
         // 用于导航栏的文本颜色。
         nav: {
-          DEFAULT: "var(--text-nav-default)",
-          hover: "var(--text-nav-hover)",
-          active: "var(--text-nav-active)",
+          DEFAULT: "rgb(var(--text-nav-default))",
+          hover: "rgb(var(--text-nav-hover))",
+          active: "rgb(var(--text-nav-active))",
         },
       },
 
       borderColor: {
-        default: "var(--border-default)",
-        primary: "var(--border-primary)",
-        secondary: "var(--border-secondary)",
+        default: "rgb(var(--border-default))",
+        primary: "rgb(var(--border-primary))",
+        secondary: "rgb(var(--border-secondary))",
       },
 
       ringColor: {
         // 用于聚焦状态下任何交互元素的聚焦环的颜色。
         focus: {
-          DEFAULT: "var(--ring-focus)",
-          destructive: "var(--ring-focus-destructive)",
+          DEFAULT: "rgb(var(--ring-focus))",
+          destructive: "rgb(var(--ring-focus-destructive))",
         },
 
         // 用于外部阴影的颜色。
         default: {
-          DEFAULT: "var(--ring-default)",
-          focus: "var(--ring-default-focus)",
+          DEFAULT: "rgb(var(--ring-default))",
+          focus: "rgb(var(--ring-default-focus))",
         },
 
         // 用于提示的边框色
         destructive: {
-          DEFAULT: "var(--ring-destructive-default)",
-          focus: "var(--ring-destructive-focus)",
+          DEFAULT: "rgb(var(--ring-destructive-default))",
+          focus: "rgb(var(--ring-destructive-focus))",
         },
       },
 
