651c32445b449aa6acc4f958e6d888c8b9174a5c
Xudong Huang <me@xudong.cc>
2024-09-18 14:39:44 +0800

    fix: 拆分为 theme.css

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,9 @@
-export default {
-  plugins: {
-    "postcss-import": {},
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+export default process.env.NODE_ENV === "production"
+  ? {}
+  : {
+      plugins: {
+        "postcss-import": {},
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    };
