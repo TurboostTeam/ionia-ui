2b88104510639e56be28fdffb1c8bdb6b95fa667
linqiuyue <me@linqiuyue.com>
2024-09-29 16:04:47 +0800

    fix: 修改导航栏文字大小

--- src/atoms/frame/frame.stories.tsx
+++ src/atoms/frame/frame.stories.tsx
@@ -7,6 +7,7 @@ import { NavigationSection } from "../../navigation-section";
 import { Page } from "../../page";
 import { Avatar, AvatarFallback, AvatarImage } from "../avatar";
 import { Header } from "../header";
+import { Menubar, MenubarMenu, MenubarTrigger } from "../menu-bar";
 import { Frame } from ".";
 
 const meta = {
@@ -44,6 +45,52 @@ export const Base: FC = () => {
       }
     >
       <Page primaryAction={{ content: "创建" }} title="页面">
+        内容
+      </Page>
+    </Frame>
+  );
+};
+
+export const TopNav: FC = () => {
+  const items = [
+    {
+      href: "#/",
+      exactMatch: true,
+      label: "概览",
+      icon: BiHome,
+    },
+    {
+      href: "#/",
+      exactMatch: true,
+      label: "登录",
+      icon: BiHome,
+    },
+  ];
+  return (
+    <Frame
+      header={
+        <Header className="flex items-center justify-between border-b px-4">
+          <div className="flex items-center">
+            <div className="mr-4">logo</div>
+            <Menubar className="border-none">
+              {items.map((item) => (
+                <MenubarMenu key={item.label}>
+                  <MenubarTrigger className="rounded">
+                    {item.label}
+                  </MenubarTrigger>
+                </MenubarMenu>
+              ))}
+            </Menubar>
+          </div>
+
+          <Avatar className="size-8">
+            <AvatarImage src="https://avatars.githubusercontent.com/u/6771141?v=4" />
+            <AvatarFallback>U</AvatarFallback>
+          </Avatar>
+        </Header>
+      }
+    >
+      <Page fullWidth primaryAction={{ content: "创建" }} title="页面">
         内容
       </Page>
     </Frame>
--- src/atoms/menu-bar/menubar-trigger.tsx
+++ src/atoms/menu-bar/menubar-trigger.tsx
@@ -3,7 +3,7 @@ import { forwardRef } from "react";
 import { tv } from "tailwind-variants";
 
 const menubarTrigger = tv({
-  base: "text-md flex cursor-default cursor-pointer select-none items-center rounded-sm px-3 py-1.5 font-medium outline-none focus:bg-nav-surface-active focus:text-nav data-[state=open]:bg-nav-surface-active data-[state=open]:text-nav",
+  base: "flex cursor-default cursor-pointer select-none items-center rounded-sm px-3 py-1.5 text-sm outline-none focus:bg-nav-surface-active focus:text-nav data-[state=open]:bg-nav-surface-active data-[state=open]:text-nav",
 });
 
 export const MenubarTrigger = forwardRef<
