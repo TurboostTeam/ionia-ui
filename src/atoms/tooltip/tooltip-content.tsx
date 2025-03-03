72e69308469f202ab3627b97afb5824146cd0f5a
cxz199705 <48152698+cxz199705@users.noreply.github.com>
2025-03-03 20:37:42 +0800

    perf(tooltip): 调整接口 (#156)
    Co-authored-by: chenxuanzong <2622009279@qq.com>

--- src/atoms/tooltip/tooltip-content.tsx
+++ src/atoms/tooltip/tooltip-content.tsx
@@ -3,7 +3,16 @@ import React from "react";
 import { tv } from "tailwind-variants";
 
 export const tooltipContent = tv({
-  base: "animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 overflow-hidden rounded-md border bg-surface px-3 py-1.5 text-xs text-default shadow-sm",
+  base: "z-50 overflow-hidden rounded-md border bg-surface px-3 py-1.5 text-xs text-default shadow-sm",
+  variants: {
+    animation: {
+      default:
+        "animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+    },
+  },
+  defaultVariants: {
+    animation: "default",
+  },
 });
 
 export interface TooltipContentProps
@@ -12,12 +21,12 @@ export interface TooltipContentProps
 export const TooltipContent = React.forwardRef<
   React.ElementRef<typeof TooltipPrimitive.Content>,
   TooltipContentProps
->(({ className, sideOffset = 8, ...props }, ref) => (
+>(({ className, sideOffset = 4, ...props }, ref) => (
   <TooltipPrimitive.Content
+    {...props}
     className={tooltipContent({ className })}
     ref={ref}
     sideOffset={sideOffset}
-    {...props}
   />
 ));
 
