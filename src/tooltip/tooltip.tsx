bee828796c6f51aa5caf0e50b984d09010526d82
chenxuanzong <2622009279@qq.com>
2025-03-03 20:29:15 +0800

    perf(tooltip): 调整接口

--- src/tooltip/tooltip.tsx
+++ src/tooltip/tooltip.tsx
@@ -3,22 +3,26 @@ import React from "react";
 import {
   Tooltip as TooltipRoot,
   TooltipContent,
+  type TooltipContentProps,
   TooltipTrigger,
 } from "../atoms/tooltip";
 
 export interface TooltipProps
   extends React.ComponentPropsWithoutRef<typeof TooltipRoot> {
   content: React.ReactNode;
+  contentConfig?: TooltipContentProps;
 }
 
 export const Tooltip = React.forwardRef<
   React.ElementRef<typeof TooltipContent>,
   TooltipProps
->(({ children, content, ...props }, ref) => {
+>(({ children, content, contentConfig, ...props }, ref) => {
   return (
     <TooltipRoot {...props}>
       <TooltipTrigger asChild>{children}</TooltipTrigger>
-      <TooltipContent ref={ref}>{content}</TooltipContent>
+      <TooltipContent ref={ref} {...contentConfig}>
+        {content}
+      </TooltipContent>
     </TooltipRoot>
   );
 });
